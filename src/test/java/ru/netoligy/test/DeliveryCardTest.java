d190abd3fdc3213b4ee711ad2aa6a793655e0bd4
cat-e <khlebalova.e@yandex.ru>
2020-08-26 01:49:28 +0400

    initial commit

---
+++ src/test/java/ru/netoligy/test/DeliveryCardTest.java
@@ -0,0 +1,45 @@
+package ru.netoligy.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.LocalDate;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DeliveryCardTest {
+
+    @Test
+    void shouldSendDataForDelivery() {
+        LocalDate date = LocalDate.now().plusDays(4);
+
+        open("http://localhost:9999/");
+        $("[placeholder='Город']").setValue("Самара");
+        $("[name='name']").sendKeys("Василий Кот");
+        $("[name='phone']").sendKeys("+79171249001");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(String.valueOf(date));
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(withText("Успешно!")).waitUntil(Condition.visible, 20000);
+
+
+    }
+}
